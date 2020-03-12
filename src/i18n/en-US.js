3904fbbe4db2f675ca71505bb1d603d2e680377d
gio <giogirecd32@gmail.com>
2020-03-12 11:38:51 -0600

    first coment
    first step to kiwi card

---
+++ src/i18n/en-US.js
@@ -0,0 +1,37 @@
+
+export const message={
+
+  KiwilimonCard:{
+        "ability": {
+            "name": "Kiwilímon",
+            "logoUrl": "https://www.kiwilimon.com/favicon-32x32.png?v=6.5.26" },
+        "content": {
+           
+            "menu": [
+                {
+                    "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/155/th5-320x320-36809.jpg",
+                    "name": "Cake ",
+                    "subtitle": "Lorenza A.",
+                    "status": ""
+                },
+                {
+                    "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/3037/th5-320x320-27155.jpg",
+                    "name": "Cheesecake",
+                    "subtitle": "D´Gari",
+                    "status": ""
+                },
+                {
+                    "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/11662/th5-320x320-18263.jpg",
+                    "name": "Pay",
+                    "subtitle": "Monserrat",
+                    "status": ""
+                }
+            ]
+        }
+    }
+        ,
+    buttonLinkArray: [{
+        "buttonText": "See more"
+    }
+    ]
+}
---
+++ src/i18n/zh-CN.js
@@ -0,0 +1,37 @@
+
+export const message={
+
+    KiwilimonCard:{
+          "ability": {
+              "name": "Kiwilímon",
+              "logoUrl": "https://www.kiwilimon.com/favicon-32x32.png?v=6.5.26" },
+          "content": {
+             
+              "menu": [
+                  {
+                      "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/155/th5-320x320-36809.jpg",
+                      "name": "粉彩 ",
+                      "subtitle": "Lorenza A.",
+                      "status": ""
+                  },
+                  {
+                      "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/3037/th5-320x320-27155.jpg",
+                      "name": "乳酪蛋糕",
+                      "subtitle": "D´Gari",
+                      "status": ""
+                  },
+                  {
+                      "imageUrl": "https://cdn.kiwilimon.com/recetaimagen/11662/th5-320x320-18263.jpg",
+                      "name": "工资",
+                      "subtitle": "Monserrat",
+                      "status": ""
+                  }
+              ]
+          }
+      }
+          ,
+      buttonLinkArray: [{
+          "buttonText": "看更多"
+      }
+      ]
+  }
